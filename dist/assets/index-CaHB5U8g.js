29384eb4fe2966c544d0446e3ea61251783fd524
mrojass <mrojas@confuturo.cl>
2024-06-11 05:33:44 -0400

    first commit

---
+++ dist/assets/index-CaHB5U8g.js
@@ -0,0 +1,2 @@
+import{o as a,p as s}from"./index-CbVhUqln.js";import{b as k}from"./index-CbVhUqln.js";function x(t){const{mockPageId:e,mockData:o}=t??{},{getKcContext:c}=a({mockData:o}),{kcContext:n}=c({mockPageId:e});return{kcContext:n}}export{a as createGetKcContext,k as createUseI18n,s as default,x as getKcContext};
+//# sourceMappingURL=index-CaHB5U8g.js.map
